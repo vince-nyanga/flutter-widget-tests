d14b78e08c46941c2d956f876374a23cbcbd6f24
Vincent Nyanga <vince.nyanga@hotmail.com>
2021-06-04 10:33:58 +0200

    added tests

--- src/test_app/test/widget_test.dart
+++ src/test_app/test/widget_test.dart
@@ -1,30 +1,46 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:test_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Should contain welcome message', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Welcome'), findsOneWidget);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+  testWidgets('Should enter and display name', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    String name = 'Vince';
+
+    expect(find.text(name), findsNothing);
+
+    await tester.enterText(find.byType(TextField), name);
+
+    await tester.tap(find.byType(ElevatedButton));
+
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text(name), findsOneWidget);
+  });
+
+  testWidgets('Should clear name', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    String name = 'Vince';
+
+    await tester.enterText(find.byType(TextField), name);
+
+    await tester.tap(find.byType(ElevatedButton));
+
+    await tester.pump();
+
+    await tester.tap(find.byType(TextButton));
+
+    await tester.pump();
+
+    expect(find.text(name), findsNothing);
   });
 }
